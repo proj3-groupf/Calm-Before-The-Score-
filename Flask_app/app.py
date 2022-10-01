3ba3fc44af60192f23dc1c56e438052210aae10a
Dantrell Person <dantrellperson2@gmail.com>
2022-10-01 13:53:35 -0400

    Updating changes, with working test js visualization

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,3 +1,4 @@
+from readline import insert_text
 from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
@@ -16,24 +17,20 @@ FIELDS = {'Date': True, 'HomeTeam': True, 'AwayTeam': True,
           'AwayScore': True, 'HomeScore': True, 'StadiumID': True, '_id': False}
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/nfl_db/season_data")
-def test_nfl():
+@app.route("/data")
+def nfl_data():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=10000)
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.close()
-
-    return jsonify(json_projects)
+    # projects = collection.find(projection=FIELDS, limit=10000)
+    result = collection.find_one({'name': 'Chart1'})
+    # values = [row[0] for row in result]
+    return jsonify({'results': result['values']})
 
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
