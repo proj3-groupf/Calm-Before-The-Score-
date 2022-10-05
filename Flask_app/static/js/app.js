c1ca90b04190056ab516ed4bdff9acfb765118b0
Dantrell Person <dantrellperson2@gmail.com>
2022-10-04 21:10:36 -0400

    Fixed js files with help from TA

--- Flask_app/static/js/app.js
+++ Flask_app/static/js/app.js
@@ -3,21 +3,53 @@
 //function to get the data and plot the charts
 function data_pull(selectionID) {
   d3.json("/data").then((data) => {
+    var gameData = data;
     console.log("NFL game data pulled from Flask API", data)
-    console.log(data[272].homeTeams);
+  var teamSelect = document.querySelector("#team_select");
+  var type = document.querySelector("#type");
+  console.log(teamSelect)
+  teamSelect.addEventListener("change", function(event) {
+    console.log(event)
+    var team = event.target.value;
+    var selectedType = type.value;
+    var homeTeamGames = gameData.filter(function(game) {
+      return team === game.HomeTeam
+    });
 
-  // pull the data into a variable
-  var data4plots = data[272].wklyScores;
-  var selectedTeam= data4plots.filter(
-    (sampleobject) => sampleobject.homeTeam == selectionID
-  )[0];
-  // Use the console to see what was saved in testSubject
-  console.log("Weekly Scores documeny from Mongo", data4plots);
-  // Setting variables needed later
-  var score = selectedTeam.scores;
-  var gameWeek = data[272].gameWeeks;
-  console.log("game weeks", gameWeek);
-  console.log("Weekly Scores", score);
+    // var homeTeamScores = homeTeamGames.map(game => game.HomeScore);
+    var selectedHomeTeamValue = homeTeamGames.map(game => game[selectedType])
+    console.log(selectedHomeTeamValue)
+
+    console.log("Home Team Games", homeTeamGames)
+  })
+  });
+
+
+  var data = {
+    // A labels array that can contain any sort of values
+    labels: [gameData.Date],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+      gameData.HomeScore
+    ]
+  };
+  
+  // As options we currently only set a static size of 300x200 px. We can also omit this and use aspect ratio containers
+  // as you saw in the previous example
+  var options = {
+    width: 300,
+    height: 200
+  };
+  
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object. As a third parameter we pass in our custom options.
+  new Chartist.Line('.ct-chart', data, options);
+
+
+}
+
+data_pull()
 
   // -------------------------------------- Creating Line Chart----------------------------------------------------------------------------
 
@@ -53,7 +85,6 @@ function optionChange(new_sample) {
 
 // Dashboard Initialization
 init();
-
 
 
 
